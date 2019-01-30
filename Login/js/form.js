d528a41886d2aaac27f4b74ea3ff1dc69ea51e0b
systems-multimedia <samuelboada@gmail.com>
2019-01-30 15:47:01 -0400

    Login and Offers design ready

---
+++ Login/js/form.js
@@ -0,0 +1,41 @@
+var register = document.querySelector('#register'),
+    login = document.querySelector('#inSession');
+
+var error = document.querySelector('.error');
+
+(function () {
+
+
+    function checkData(e) {
+        if (login.email.value == 0) {
+            error.innerHTML = '*Insert your email';
+            e.preventDefault();
+            return false;
+        } else if (login.email.value != 'admin@tangerine.com') {
+            error.innerHTML = '*Unknown User';
+            e.preventDefault();
+            return false;
+        } else {
+            if (login.password.value != 'admin') {
+                error.innerHTML = '*Wrong Password';
+                e.preventDefault();
+                return false;
+            }
+        }
+
+        e.preventDefault();
+        document.location.href = "../html/dashboard.html";
+    }
+
+    function checkRegister(e) {
+
+    }
+
+
+    function validate(e) {
+        checkData(e);
+    }
+
+    login.addEventListener('submit', validate);
+    register.addEventListener('submit', checkRegister);
+}());
